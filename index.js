a0adba8172930deeb310db57bdf5beff1a426ead
chelseaayoo <chelsea.ayoo@student.moringaschool.com>
2021-07-18 23:42:19 +0300

    add modifications to index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function onFormSubmit(FormData) {
     let dayOfTheWeek = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
     return dayOfTheWeek;
     let day = Math.trunc(dayOfTheWeek);
-    console.log(dayOfTheWeek)
+    console.log(day)
     
 }
-console.log[dayOfTheWeek];
-return dayOfTheWeek;
+console.log(day);
+return day;
 const name = ['kwasi','kwadwo','kwabena','kwaku','yaw','kofi','kwame','akosua','adwoa','abenaa','akua','yaa','afua','ama'];
 console.log(names.join('-'));//
 const gender= document.querySelector('input[name="gender"]:checked').value;
@@ -14,45 +14,45 @@ console.log(gender)
 //const gender=document.getElementById("gender").value;
 function onFormSubmit(formData) {
     var formData = readFormData();
-    if (dayOfTheWeek = 0 && gender === male ){
+    if (day = 0 && gender === male ){
         console.log("kwasi");
         
     }else if (gender === female){
         console.log("akosua")
     }else{
         console.log("ama")
-    }if (dayOfTheWeek= 1 && gender===male){
+    }if (day= 1 && gender===male){
         console.log("kwadwo");
     }else if (gender=== female){
         console.log("adwoa");
     }else{
         console.log("afua")
     }
-    if (dayOfTheWeek=2 && gender===male){
+    if (day=2 && gender===male){
         console.log("kwabena")
     }else if (gender===female){
         console.log("abenaa")
     }else{
         console.log("yaa")
-    }if (dayOfTheWeek=3&& gender===male){
+    }if (day=3&& gender===male){
         console.log("kwaku")
     }else if(gender ===female){
         console.log("akua")
     }else{
         console.log("kwasi")
-    }if (dayOfTheWeek= 4 && gennder=== male){
+    }if (day= 4 && gennder=== male){
         console.log("yaw")
     }else if (gender ===female){
         console.log("yaa")
     }else{
         console.log("kwadwo")
-    }if (dayOfTheWeek= 5 && gender === male){
+    }if (day= 5 && gender === male){
         console.log("kofi")
     }else if (gender === female){
         console.log("afua")
     }else{
         console.log("kwabena")
-    }if(dayOfTheWeek===6 && gender ===male){
+    }if(day===6 && gender ===male){
         console.log("kwame")
     }else if (gender === female){
         console.log("ama")
